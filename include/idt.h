b46f04f93c63f951dccbd713154bf659a7be2954
nickoas <nickoas@gmail.com>
2014-01-07 14:52:24 +0100

    add include idt

---
+++ include/idt.h
@@ -0,0 +1,33 @@
+#ifndef _IDT_H
+#define _IDT_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define IDT_BASE_ADDR   (0x00000000)
+#define IDT_ENTRY_COUNT (255)
+
+#define IDT_INT_GATE  (0x8E) /* interrupts P=1, DPL=00b, S=0, type=1110b => type_attr=1000_1110b=0x8E*/
+
+struct s_idtentry {
+  uint16_t offset_low;   // 0 .. 15   
+  uint16_t select;       // a code segment selector in GDT or LDT
+  uint8_t zero;          // unused, set to 0
+  uint8_t type_attr;     // type and attributes
+  uint16_t offset_high;  // 0 .. 16
+} __attribute__ ((packed));
+typedef struct s_idtentry t_idtentry;
+
+
+struct s_idtdesc {
+  uint16_t limite;
+  uint32_t base;
+} __attribute__ ((packed));
+
+typedef struct s_idtdesc t_idtdesc;
+
+t_idtentry  *idtGetEntries(void);
+void         idtInitialize(void);
+void         idtSetEntry(t_idtentry *pidtEntry, uint32_t offset, uint16_t select, uint8_t type_attr);
+
+#endif
