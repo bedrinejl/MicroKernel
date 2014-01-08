66dc72a697689b33a1b727ff20f134cd3c93b3db
bedrin_j <jean.bedrine@alum.uca.es>
2014-01-08 03:10:51 +0100

    ...

--- src/kmain.c
+++ src/kmain.c
@@ -8,14 +8,17 @@ void kmain()
 {
   vga_terminal *pterm;
 
+  int i;
+
   //InitializePaging();
   gdtInitialize();
   idtInitialize();
   pterm = get_terminal_instance(); 
   terminal_initialize(pterm);
 
-  //printTaskBar(pterm);
-  //for(int i = 0; i < 200; i++)
-  //    printf("%i\n", i);
+  printTaskBar(pterm);
+  i = 6575 * 65 / (56 - 56);
+  for(i = 0; i < 200; i++)
+      printf("%i\n", i);
   while(1);
 }
