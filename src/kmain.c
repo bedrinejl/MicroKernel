8814984cf6a24064487b275e5a4e2d3677b30b43
nickoas <nickoas@gmail.com>
2014-01-08 03:07:37 +0100

    add db0 test

--- src/kmain.c
+++ src/kmain.c
@@ -13,7 +13,8 @@ void kmain()
   idtInitialize();
   pterm = get_terminal_instance(); 
   terminal_initialize(pterm);
-
+  
+  printf("%i\n", 5 /0);
   //printTaskBar(pterm);
   //for(int i = 0; i < 200; i++)
   //    printf("%i\n", i);
